3413496e5224a5c4905e0b88ad465a088c375503
Clucas0311 <clucas0311@gmail.com>
2021-02-28 12:16:00 -0500

    SetUp of App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			{/* todo list */}
+			{/* done */}
+		</div>
+	);
+}
+
+export default App;
